163d22a9edcac9a7c1789d5466ece319a491b09b
Diyar NGI <diyarngi@Diyars-MacBook-Pro.local>
2023-06-09 14:26:09 +0700

    fix: load & insert jalur

--- app/Modules/Administrator/Views/manjalur.php
+++ app/Modules/Administrator/Views/manjalur.php
@@ -1,10 +1,14 @@
+<!-- style internal -->
 <style>
     .select2-container {
         width: 100% !important;
     }
 </style>
+
+<!-- content -->
 <div>
-    <div class="page-hero page-container " id="page-hero">
+    <!-- title -->
+    <div class="page-hero page-container" id="page-hero">
         <div class="padding d-flex">
             <div class="page-title">
                 <h2 class="text-md text-highlight"><?= $page_title ?></h2>
@@ -12,15 +16,17 @@
             <div class="flex"></div>
         </div>
     </div>
-    <div class="page-content page-container" id="page-content">
+
+    <!-- body -->
+    <div class="container page-content page-container" id="page-content">
         <div class="card">
             <div class="card-header">
                 <ul class="nav nav-pills card-header-pills no-border" id="tab">
                     <li class="nav-item">
-                        <a class="nav-link active" data-toggle="tab" href="#tab-data" role="tab" aria-controls="tab-data" aria-selected="false"><i class="fa fa-table"></i> Data</a>
+                        <a class="nav-link active" id="nav-data" data-toggle="tab" href="#tab-data" role="tab" aria-controls="tab-data" aria-selected="false"><i class="fa fa-table"></i> Data</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" data-toggle="tab" href="#tab-form" role="tab" aria-controls="tab-form" aria-selected="false"><i class="fa fa-plus"></i> Form</a>
+                        <a class="nav-link" id="nav-form" data-toggle="tab" href="#tab-form" role="tab" aria-controls="tab-form" aria-selected="false"><i class="fa fa-plus"></i> Form</a>
                     </li>
                 </ul>
             </div>
@@ -33,7 +39,8 @@
                                     <thead>
                                         <tr>
                                             <th><span>#</span></th>
-                                            <th><span>Jalur</span></th>
+                                            <th><span>Type</span></th>
+                                            <th><span>Koridor</span></th>
                                             <th><span>Rute</span></th>
                                             <th><span>Trip A</span></th>
                                             <th><span>Trip B</span></th>
@@ -51,27 +58,33 @@
                                 <input type="hidden" class="form-control" id="id" name="id" value="" required>
                                 <?= csrf_field(); ?>
                                 <div class="form-group row">
+                                    <label for="type_bis_id" class="col-2">Jenis Bis</label>
+                                    <div class="col-10">
+                                        <select class="form-control sel2" id="type_bis_id" name="type_bis_id" placeholder="Pilih jenis bis" required></select>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
                                     <label for="koridor" class="col-2">Jalur</label>
                                     <div class="col-10">
-                                        <input class="form-control" type="text" placeholder="Jalur menggunakan romawi huruf besar" id="koridor" name="koridor" required="" />
+                                        <input class="form-control" type="text" placeholder="Jalur menggunakan romawi huruf besar" id="koridor" name="koridor" autocomplete="off" required />
                                     </div>
                                 </div>
                                 <div class="form-group row">
                                     <label for="rute" class="col-2">Rute</label>
                                     <div class="col-10">
-                                        <input class="form-control" type="text" placeholder="Contoh: Tangerang-Semarang" id="rute" name="rute" required/>
+                                        <input class="form-control" type="text" placeholder="Contoh: Terminal Poris Plawad - Jl. Gatot Subroto" id="rute" name="rute" autocomplete="off" required/>
                                     </div>
                                 </div>
                                 <div class="form-group row">
-                                    <label for="trip_a" class="col-2">Trip A</label>
+                                    <label for="origin" class="col-2">Trip A</label>
                                     <div class="col-10">
-                                        <input class="form-control" type="text" placeholder="Posisi Awal" id="trip_a" name="trip_a" required/>
+                                        <input class="form-control" type="text" placeholder="Posisi Awal" id="origin" name="origin" autocomplete="off" required/>
                                     </div>
                                 </div>
                                 <div class="form-group row">
-                                    <label for="trip_b" class="col-2">Trip B</label>
+                                    <label for="toward" class="col-2">Trip B</label>
                                     <div class="col-10">
-                                        <input class="form-control" type="text" placeholder="Posisi Akhir" id="trip_b" name="trip_b" required/>
+                                        <input class="form-control" type="text" placeholder="Posisi Akhir" id="toward" name="toward" autocomplete="off" required/>
                                     </div>
                                 </div>
                                 <div class="form-group row">
@@ -80,13 +93,13 @@
                                         <input type="color" class="form-control" id="color" name="color" placeholder="#000000" required/>
                                     </div>
                                 </div>
+                                <hr class="mt-3">
+                                <div class="text-left mt-3 offset-2">
+                                    <button type="button" class="btn btn-secondary w-sm" id="batal">Batal</button>
+                                    &nbsp;
+                                    <button type="submit" class="btn btn-primary w-sm" id="simpan">Simpan</button>
+                                </div>
                             </form>
-                            <hr class="mt-3">
-                            <div class="text-left mt-3 offset-2">
-                                <button type="button" class="btn btn-secondary w-sm" id="batal">Batal</button>
-                                &nbsp;
-                                <button type="submit" class="btn btn-primary w-sm" id="simpan">Simpan</button>
-                            </div>
                         </div>
                     </div>
                 </div>
@@ -94,6 +107,8 @@
         </div>
     </div>
 </div>
+
+<!-- script internal -->
 <script type="text/javascript">
     const auth_insert = '<?= $rules->i ?>';
     const auth_edit = '<?= $rules->e ?>';
@@ -107,53 +122,49 @@
     var dataStart = 0;
     var coreEvents;
 
+    // init select2
     const select2Array = [{
-        id: 'cekpos',
-        url: '/findkelompokpos',
-        placeholder: 'Pilih Pos',
+        id: 'type_bis_id',
+        url: '/type_bis_id_select_get',
+        placeholder: 'Pilih Type Bis',
         params: null
     }];
 
     $(document).ready(function() {
-        var now = new Date();
-        var day = ("0" + now.getDate()).slice(-2);
-        var month = ("0" + (now.getMonth() + 1)).slice(-2);
-        var today = now.getFullYear() + "-" + (month) + "-" + (day);
-        console.log(today);
-        $('#dob').val(today);
-        $('#dob').datepicker({
-            format: 'yyyy-mm-dd',
-            autoclose: true,
-            todayHighlight: true,
-            endDate: today,
-            startView: 2,
-            orientation: "bottom auto",
-        });
-
+        // init core event
         coreEvents = new CoreEvents();
         coreEvents.url = url;
         coreEvents.ajax = url_ajax;
         coreEvents.csrf = {
             "<?= csrf_token() ?>": "<?= csrf_hash() ?>"
         };
-        // coreEvents.tableColumn = datatableColumn();
 
+        // datatable load
+        coreEvents.tableColumn = datatableColumn();
+
+        // insert
         coreEvents.insertHandler = {
-            placeholder: 'Data pegawai berhasil ditambahkan',
+            placeholder: 'Data jalur berhasil ditambahkan',
             afterAction: function(result) {
-                // $('#tab-data').addClass('active show');
-                // $('#tab-form').removeClass('active show');
-                // coreEvents.table.ajax.reload();
+                $('#tab-data').addClass('active show');
+                $('#tab-form').removeClass('active show');
+                $('#nav-data').addClass('active');
+                $('#nav-form').removeClass('active');
+
+                $(".sel2").val(null).trigger('change');
+
+                coreEvents.table.ajax.reload();
             }
         }
 
+        // update
         coreEvents.editHandler = {
-            placeholder: 'Data pegawai berhasil diubah',
+            placeholder: 'Data jalur berhasil diubah',
             afterAction: function(result) {}
         }
 
         coreEvents.deleteHandler = {
-            placeholder: 'Data pegawai berhasil dihapus',
+            placeholder: 'Data jalur berhasil dihapus',
             afterAction: function() {}
         }
 
@@ -168,53 +179,74 @@
         select2Array.forEach(function(x) {
             coreEvents.select2Init('#' + x.id, x.url, x.placeholder, x.params);
         });
-        // select2Array.forEach(function(x) {
-        //     select2Init('#' + x.id, x.url, x.placeholder, x.params);
-        // });
-
+        
         coreEvents.load(null, [0, 'asc'], null);
     });
 
-    // function datatableColumn() {
-    //     let columns = [{
-    //             data: "id",
-    //             orderable: false,
-    //             width: 100,
-    //             render: function(a, type, data, index) {
-    //                 return dataStart + index.row + 1
-    //             }
-    //         },
-    //         {
-    //             data: "fullname",
-    //             orderable: true
-    //         },
-    //         {
-    //             data: "telp",
-    //             orderable: true
-    //         },
-    //         {
-    //             data: "id",
-    //             orderable: false,
-    //             width: 100,
-    //             render: function(a, type, data, index) {
-    //                 let button = ''
+    // datatable column
+    function datatableColumn() {
+        let columns = [{
+                data: "id",
+                orderable: false,
+                width: 100,
+                render: function(a, type, data, index) {
+                    return dataStart + index.row + 1
+                }
+            },
+            {
+                data: "m_type_bis_nama",
+                orderable: true
+            },
+            {
+                data: "koridor",
+                orderable: true
+            },
+            {
+                data: "rute",
+                orderable: true
+            },
+            {
+                data: "origin",
+                orderable: true
+            },
+            {
+                data: "toward",
+                orderable: true
+            },
+            {
+                data: "color",
+                orderable: false,
+                render: function(a, type, data, index) {
+                    return `<div style="border: 1px solid black; 
+                                        width: 50px; 
+                                        height: 25px; 
+                                        background-color: ${data.color} "></div>`
+                }
+            },
+            {
+                data: "id",
+                orderable: false,
+                width: 100,
+                render: function(a, type, data, index) {
+                    let button = ''
 
-    //                 if (auth_edit == "1") {
-    //                     button += '<button class="btn btn-sm btn-outline-primary edit" data-id="' + data.id + '" title="Edit">\
-    //                                 <i class="fa fa-edit"></i></button>';
-    //                 }
+                    if (auth_edit == "1") {
+                        button += '<button class="btn btn-sm btn-outline-primary edit" data-id="' + data.id + '" title="Edit">\
+                                    <i class="fa fa-edit"></i></button>';
+                    }
 
-    //                 if (auth_delete == "1") {
-    //                     button += '<button class="btn btn-sm btn-outline-danger delete" data-id="' + data.id + '" title="Delete">\
-    //                                     <i class="fa fa-trash-o"></i></button></div>';
-    //                 }
+                    if (auth_delete == "1") {
+                        button += '<button class="btn btn-sm btn-outline-danger delete" data-id="' + data.id + '" title="Delete">\
+                                        <i class="fa fa-trash-o"></i></button></div>';
+                    }
 
-    //                 button += (button == '') ? "<b>Tidak ada aksi</b>" : ""
+                    button += (button == '') ? "<b>Tidak ada aksi</b>" : ""
 
-    //                 return button;
-    //             }
-    //         }
-    //     ];
-    //     return columns;
-    // }
+                    return button;
+                }
+            }
+        ];
+        return columns;
+    }
+
 </script>
