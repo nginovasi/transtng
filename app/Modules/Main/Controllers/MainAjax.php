80b44c536d20f32111f15a442222fd31165b6efa
reycaruzz <koesanto.r@gmail.com>
2023-06-23 18:28:00 +0700

    dashboard lagi slurr

--- app/Modules/Main/Controllers/MainAjax.php
+++ app/Modules/Main/Controllers/MainAjax.php
@@ -35,6 +35,7 @@ class MainAjax extends BaseController
                             WHEN jenis LIKE '%BRIZZI%' THEN SUBSTRING(jenis, 1, 7)
                             WHEN jenis LIKE '%E-Money%' THEN SUBSTRING(jenis, 1, 7)
                             WHEN jenis LIKE '%FLAZZ%' THEN SUBSTRING(jenis, 1, 6)
+                            WHEN jenis LIKE '%Tapcash%' THEN SUBSTRING(jenis, 1, 8)
                             ELSE jenis
                         END AS jenis_transaksi,
                         SUM(CASE WHEN tanggal = CURDATE() THEN 1 ELSE 0 END) AS total_penumpang,
@@ -67,8 +68,23 @@ class MainAjax extends BaseController
         echo json_encode($data);
     }
 
+    public function getAllTransaksiJalur() {
+        $query = "SELECT a.id, a.jalur, a.rute, a.color, IFNULL(SUM(b.kredit), 0) AS ttl_pendapatan_jalur, IFNULL(count(b.no_trx), 0) AS ttl_penumpang_jalur
+                    FROM ref_jalur a
+                    LEFT JOIN transaksi_bis b ON a.id = b.jalur AND DATE(b.tanggal) = CURDATE()
+                    WHERE a.is_deleted = 0
+                    GROUP BY a.id;";
+        $rs = $this->db->query($query)->getResult();
+        if($rs) {
+            $data = ['success' => TRUE, 'message' => 'Data berhasil ditemukan', 'data' => $rs ];
+        } else {
+            $data = ['success' => FALSE, 'message' => 'Data tidak ditemukan', 'data' => [] ];
+        }
+        echo json_encode($data);
+    }
+
     public function getAllDevice() {
-        $query = "SELECT COUNT(*) AS ttl_tob, SUM(CASE WHEN is_power = 'off' THEN 1 ELSE 0 END) AS ttl_tob_offline
+        $query = "SELECT COUNT(*) AS ttl_tob, SUM(CASE WHEN is_power = 'on' THEN 1 ELSE 0 END) AS ttl_tob_online
                     FROM ref_haltebis
                     WHERE is_deleted = 0";
         $rs = $this->db->query($query)->getRow();
@@ -85,6 +101,20 @@ class MainAjax extends BaseController
                     FROM ref_jalur a
                     WHERE a.is_deleted = 0";
         $rs = $this->db->query($query)->getRow();
+        if($rs) {
+            $data = ['success' => TRUE, 'message' => 'Data berhasil ditemukan', 'data' => $rs ];
+        } else {
+            $data = ['success' => FALSE, 'message' => 'Data tidak ditemukan', 'data' => [] ];
+        }
+        echo json_encode($data);
+    }
+
+    public function sumTransaksi30() {
+        $query = "SELECT DATE(tanggal) AS tgl_transaksi, SUM(kredit) AS sum_value, COUNT(no_trx) AS sum_penumpang
+                    FROM transaksi_bis
+                    WHERE tanggal >= CURDATE() - INTERVAL 30 DAY
+                    GROUP BY DATE(tanggal)";
+        $rs = $this->db->query($query)->getResultArray();
         if($rs) {
             $data = ['success' => TRUE, 'message' => 'Data berhasil ditemukan', 'data' => $rs ];
         } else {
