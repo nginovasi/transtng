5d017b1ef48accf99e6af5e3ffa9e5c5d608dd27
Diyar NGI <diyarngi@Diyars-MacBook-Pro.local>
2023-06-26 12:43:11 +0700

    fix: refactor transaksi per jam jalur

--- app/Modules/Eksekutif/Views/trxjamjalur.php
+++ app/Modules/Eksekutif/Views/trxjamjalur.php
@@ -24,14 +24,14 @@
             <div class="card-header">
                 <ul class="nav nav-pills card-header-pills no-border" id="tab">
                     <li class="nav-item">
-                        <a class="nav-link active" data-toggle="tab" href="#tab-data-jamjalur" role="tab" aria-controls="tab-data-jamjalur" aria-selected="true"><i class="fa fa-calendar" aria-hidden="true"></i> Data</a>
+                        <a class="nav-link active" data-toggle="tab" href="#tab-data" role="tab" aria-controls="tab-data" aria-selected="true"><i class="fa fa-calendar" aria-hidden="true"></i> Data</a>
                     </li>
                 </ul>
             </div>
             <div class="card-body">
                 <div class="padding">
                     <div class="tab-content">
-                        <div class="tab-pane fade active show" id="tab-data-trxjamjalur" role="tabpanel" aria-labelledby="tab-data-trxjamjalur">
+                        <div class="tab-pane fade active show" id="tab-data" role="tabpanel" aria-labelledby="tab-data">
                             <div class="form-group row">
                                 <div class="input-group mb-3 col-md-3">
                                     <div class="input-group">
@@ -40,7 +40,7 @@
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar mx-2"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>
                                             </span>
                                         </div>
-                                        <input type="text" class="form-control form-control-md date" name="date-trxjamjalur" id="date-trxjamjalur" placeholder="Masukkan Tanggal" required autocomplete="off">
+                                        <input type="text" class="form-control form-control-md date" name="date" id="date" placeholder="Masukkan Tanggal" required autocomplete="off">
                                     </div>
                                 </div>
                                 <div class="input-group mb-3 col-md-3">
@@ -50,11 +50,11 @@
                                     <button class="btn btn-success btn-transaction" id="btn-transaction">Lihat Transaksi</button>
                                 </div>
                                 <div class="mb-2 col-md-2">
-                                    <div class="btn-group-export" style="display: none;">
+                                    <div class="btn-group" style="display: none;">
                                         <button class="btn btn-white">Export</button>
                                         <button class="btn btn-white dropdown-toggle" data-toggle="dropdown" aria-expanded="false"></button>
                                         <div class="dropdown-menu bg-dark" role="menu" x-placement="bottom-start" style="position: absolute; transform: translate3d(93px, 34px, 0px); top: 0px; left: 0px; will-change: transform;">
-                                            <a class="dropdown-item" id="download-data-pdf">
+                                            <a class="dropdown-item" id="download-pdf">
                                                 PDF
                                             </a>    
                                             <!-- <a class="dropdown-item">
@@ -67,7 +67,11 @@
 
                             <hr>
 
-                            <div id="statistik-rekap-data"></div>
+                            <div class="tab-content-header my-3" style="display: block;">    
+                                <h6 class="tab-content-title text-center font-weight-bold"></h6>
+                            </div>
+
+                            <div id="statistik-rekap"></div>
                         </div>
                     </div>
                 </div>
@@ -86,7 +90,7 @@
     const base_url = '<?= base_url() ?>';
     const url = '<?= base_url() . "/" . uri_segment(0) . "/action/" . uri_segment(1) ?>';
     const url_ajax = '<?= base_url() . "/" . uri_segment(0) . "/ajax" ?>';
-    const url_pdf_jalur = '<?= base_url() . "/" . uri_segment(0) . "/pdf/exportTrxPenumpangPerJamJalur" . "" ?>';
+    const url_pdf = '<?= base_url() . "/" . uri_segment(0) . "/pdf/exportTrxPerJamJalur" . "" ?>';
 
     var dataStart = 0;
     var coreEvents;
@@ -135,23 +139,31 @@
         
         // coreEvents.load(null, [0, 'asc'], null);
 
-        coreEvents.daterangepicker('#date-trxjamjalur', 'yyyy-mm-dd')
+        coreEvents.daterangepicker('#date', 'yyyy-mm-dd')
 
-        $('#download-data-pdf').on('click', function(e) {
-            let date = $('#date-trxjamjalur').val()
-            let jalur = $('#jalur_id').val()
-
-            $(this).attr("href", url_pdf_jalur + '?date=' + date + '&jalur_id=' + jalur);
-            $(this).attr("target", "_blank");
+        $('#date').on('apply.daterangepicker', function(ev, picker) {
+            $(this).val(picker.startDate.format('YYYY-MM-DD') + ' - ' + picker.endDate.format('YYYY-MM-DD'));
         });
 
-        $('#jenpos_id').select2()
+        $('#date').on('cancel.daterangepicker', function(ev, picker) {
+            $(this).val('');
+        });
 
+        $('#download-pdf').on('click', function(e) {
+            let date = $('#date').val()
+            let jalur = $('#jalur_id').val()
+            let jalurText = $('#jalur_id').find(":selected").text()
+
+            $(this).attr("href", url_pdf + '?date=' + date + '&jalur_id=' + jalur + '&jalur_text=' + jalurText);
+            $(this).attr("target", "_blank");
+        });
     });
 
     $('#btn-transaction').on('click', function() {
-        let date = $('#date-trxjamjalur').val()
+        let date = $('#date').val()
         let jalur = $('#jalur_id').val()
+
+        loaderStart()
 
         $.ajax({
             method: "post",
@@ -165,9 +177,26 @@
             success : function (rs) {
 
                 if(rs.success){
-                    $('#statistik-rekap-data').html('')
+                    $('#statistik-rekap').html('')
 
-                    $('.btn-group-export').css('display', 'block')
+                    $('.btn-group').css('display', 'block')
+
+                    let title = `REKAP LAPORAN TRANSAKSI `
+                    if(date) {
+                        let explodeDateHarian = date.split(' - ');
+                        let explodeDateStart = explodeDateHarian[0].split('-');
+                        let explodeDateEnd = explodeDateHarian[1].split('-');
+
+                        title += `PERIODE ${explodeDateStart[2]} ${getMonth(explodeDateStart[1])} ${explodeDateStart[0]} - ${explodeDateEnd[2]} ${getMonth(explodeDateEnd[1])} ${explodeDateEnd[0]} `
+                    }
+
+                    if(jalur) {
+                        title += $('#jalur_id').find(":selected").text()
+                    }
+
+                    $(".tab-content-title").text("")
+
+                    $(".tab-content-title").text(`${title}`)
 
                     let result = `
                         <div class="table-responsive">
@@ -211,8 +240,9 @@
                         </div>
                     `
 
-                    $('#statistik-rekap-data').append(result)
+                    $('#statistik-rekap').append(result)
 
+                    loaderEnd()
                 }
 
             }
@@ -221,6 +251,52 @@
 
     function numberWithCommas(x) {
         return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+
+    function loaderStart() {
+        Swal.fire({
+            title: "",
+            icon: "info",
+            text: "Proses menampilkan data, mohon ditunggu...",
+            didOpen: function() {
+                Swal.showLoading()
+            }
+        });
+    }
+
+    function loaderEnd() {
+        swal.close();
+    }
+
+    function getMonth(month) { 
+        let result
+        if(month == 01) {
+            result = "JANUARI"
+        } else if(month == 02) {
+            result = "FEBRUARI"
+        } else if(month == 03) {
+            result = "MARET"
+        } else if(month == 04) {
+            result = "APRIL"
+        } else if(month == 05) {
+            result = "MEI"
+        } else if(month == 06) {
+            result = "JUNI"
+        } else if(month == 07) {
+            result = "JULI"
+        } else if(month == 08) {
+            result = "AGUSTUS"
+        } else if(month == 09) {
+            result = "SEPTEMBER"
+        } else if(month == 10) {
+            result = "OKTOBER"
+        } else if(month == 11) {
+            result = "NOVEMBER"
+        } else {
+            result = "DESEMBER"
+        }
+
+        return result
     }
 
 </script>
